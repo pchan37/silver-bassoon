dab1f54dd63f7b801ceb23c968c688d918f24ed0
Patrick Chan <patrickchan1999@gmail.com>
2017-02-13 23:38:38 -0500

    Finished assignment

---
+++ canvas-drawing.js
@@ -0,0 +1,19 @@
+var clearButton = document.getElementById("clear-drawing");
+var canvas = document.getElementById("canvas");
+
+clearButton.addEventListener("click", function(e){
+    var canvasContext = canvas.getContext('2d');
+    canvasContext.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+    alert("As you were dreaming, the disco bandits wiped everything...");
+});
+
+canvas.addEventListener("click", function(e){
+    var canvasContext = canvas.getContext('2d');
+    var thickness = 5;
+    var rectangle = canvas.getBoundingClientRect();
+    var xcor = e.clientX - rectangle.left - 0.5 * thickness;
+    var ycor = e.clientY - rectangle.top - 0.5 * thickness;
+
+    canvasContext.fillRect(xcor, ycor, thickness, thickness);
+});		       
