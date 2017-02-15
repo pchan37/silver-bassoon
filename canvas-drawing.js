572f3e1f718be9462bb7cf8dddeb164003142267
Patrick Chan <patrickchan1999@gmail.com>
2017-02-14 22:53:35 -0500

    Added connect to dots

--- canvas-drawing.js
+++ canvas-drawing.js
@@ -6,9 +6,10 @@ clearButton.addEventListener("click", function(e){
     canvasContext.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
 
     alert("As you were dreaming, the disco bandits wiped everything...");
+    canvasContext.beginPath();
 });
 
-canvas.addEventListener("click", function(e){
+var drawRectangle = function(e){
     var canvasContext = canvas.getContext('2d');
     var thickness = 5;
     var rectangle = canvas.getBoundingClientRect();
@@ -16,4 +17,21 @@ canvas.addEventListener("click", function(e){
     var ycor = e.clientY - rectangle.top - 0.5 * thickness;
 
     canvasContext.fillRect(xcor, ycor, thickness, thickness);
-});		       
+};		       
+
+var canvasContext = canvas.getContext("2d");
+
+var connectDots = function(e){
+    var xcor = e.offsetX;
+    var ycor = e.offsetY;
+
+    canvasContext.fillStyle = "LightSteelBlue";
+    canvasContext.lineTo(xcor, ycor);
+    canvasContext.stroke();
+    canvasContext.beginPath();
+    canvasContext.arc(xcor, ycor, 5, 0, Math.PI * 2);
+    canvasContext.fill();
+    canvasContext.moveTo(xcor, ycor);
+};
+
+canvas.addEventListener("click", connectDots);
